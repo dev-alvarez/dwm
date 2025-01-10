acc639df9361768fb1698b6d1cb0a381344f3b71
dev-alvarez <A480041724@my.uvm.edu.mx>
2025-01-09 22:18:54 -0600

    commit

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 0;  /**/        /* border pixel of windows */
 static const unsigned int gappx     = 0;   /**/     /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
