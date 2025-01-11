897e4944557e22d8fb9e1a0ee60b9d86c825dae2
dev-alvarez <A480041724@my.uvm.edu.mx>
2025-01-10 19:05:55 -0600

    ChangedColorsToBlack

--- config.h
+++ config.h
@@ -4,24 +4,26 @@
 static const unsigned int borderpx  = 0;  /**/        /* border pixel of windows */
 static const unsigned int gappx     = 0;   /**/     /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char normbgcolor[] = "#000000";  // Black background
+static const char normfgcolor[] = "#FFFFFF";  // White text
+static const char normbordercolor[] = "#000000"; // Black border
+static const char selbgcolor[]  = "#000000";  // Black background for selected item
+static const char selfgcolor[]  = "#FFFFFF";  // White text for selected item
+static const char selbordercolor[] = "#FFFFFF"; // White border for focused window
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg         bg         border   */
+    [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+    [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -63,7 +65,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { 
+    "dmenu_run", "-m", dmenumon, 
+    "-fn", dmenufont, 
+    "-nb", normbgcolor,  // Normal background (e.g., black)
+    "-nf", normfgcolor,  // Normal foreground (e.g., white)
+    "-sb", "#005577",    // Selected background (e.g., blue)
+    "-sf", "#ffffff",    // Selected foreground (e.g., white)
+    NULL 
+};
+
 static const char *termcmd[]  = { "st", NULL };
 static const char *upbrightness[]   = { "xbacklight", "-inc", "10", NULL };
 static const char *downbrightness[] = { "xbacklight", "-dec", "10", NULL };
